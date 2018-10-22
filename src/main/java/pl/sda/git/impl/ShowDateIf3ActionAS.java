456ec7d4279bf2e6628eea96e79f11e5ffb0037e
arqarq <43341071+arqarq@users.noreply.github.com>
2018-10-22 23:56:21 +0200

    - pokazywanie daty uzywa klas LocalDate i DateTimeFormatter

--- src/main/java/pl/sda/git/impl/ShowDateIf3ActionAS.java
+++ src/main/java/pl/sda/git/impl/ShowDateIf3ActionAS.java
@@ -2,14 +2,29 @@ package pl.sda.git.impl;
 
 import pl.sda.git.Action;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 public class ShowDateIf3ActionAS implements Action {
     public String doIt(List<String> actionInput) {
         if (actionInput.size() == 3) {
-            StringBuilder date = new StringBuilder();
-            date.append(actionInput.get(0)).append('.').append(actionInput.get(1)).append('.').append(actionInput.get(2)).trimToSize();
-            return date.toString();
+            StringBuilder date = new StringBuilder()
+                    .append(actionInput.get(0))
+                    .append('-')
+                    .append(actionInput.get(1))
+                    .append('-')
+                    .append(actionInput.get(2));
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("d-M-y");
+            LocalDate localDate;
+            try {
+                localDate = LocalDate.parse(date, dateTimeFormatter);
+            } catch (DateTimeParseException ex) {
+                return "Nic z tym nie zrobię, bo niewłaściwe dane wej.";
+            }
+            dateTimeFormatter = DateTimeFormatter.ofPattern("d.MM.yyyy");
+            return localDate.format(dateTimeFormatter);
         }
         return "Nic z tym nie zrobię.";
     }
