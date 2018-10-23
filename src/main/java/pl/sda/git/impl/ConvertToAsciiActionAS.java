d98ca2f080049256a5f8cd885ef0d0bcc791bd37
arqarq <43341071+arqarq@users.noreply.github.com>
2018-10-23 21:04:03 +0200

    - zmiany w ConvertToAsciiActionAS.java

--- src/main/java/pl/sda/git/impl/ConvertToAsciiActionAS.java
+++ src/main/java/pl/sda/git/impl/ConvertToAsciiActionAS.java
@@ -3,6 +3,7 @@ package pl.sda.git.impl;
 import pl.sda.git.Action;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class ConvertToAsciiActionAS implements Action {
     public String doIt(List<String> actionInput) {
@@ -13,6 +14,17 @@ public class ConvertToAsciiActionAS implements Action {
         for (char znak : allChars) {
             sb.append((int) znak).append("d ");
         }
+
+        final String revertedAsciiValuesOfStrings = Stream.of("aaaa", "bddd", "c")
+                .flatMapToInt(String::chars)
+           //     .map(String::valueOf)
+                .mapToObj(StringBuilder::new)
+                .reduce(StringBuilder::append)
+                .map(StringBuilder::reverse)
+                .map(StringBuilder::toString)
+                .orElse("");
+        System.out.println(revertedAsciiValuesOfStrings);
+
 //        sb.deleteCharAt(sb.length() - 1); //skasowanie spacji na koncu
 //        sb.delete(sb.length() - 1, sb.length()); //skasowanie spacji na koncu
         return sb.toString().trim(); // skasowanie spacji na koncu
