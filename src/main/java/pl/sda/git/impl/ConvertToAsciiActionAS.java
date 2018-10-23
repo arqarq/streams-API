1f120363bd06e697327acd128844f055a7b70019
arqarq <43341071+arqarq@users.noreply.github.com>
2018-10-23 23:57:58 +0200

    - zmiany w ConvertToAsciiActionAS.java i Cut3ActionAS.java

--- src/main/java/pl/sda/git/impl/ConvertToAsciiActionAS.java
+++ src/main/java/pl/sda/git/impl/ConvertToAsciiActionAS.java
@@ -3,30 +3,28 @@ package pl.sda.git.impl;
 import pl.sda.git.Action;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 public class ConvertToAsciiActionAS implements Action {
     public String doIt(List<String> actionInput) {
-        String allInputAsString = String.join(" ", actionInput); // z powrotem calosc w Stringu
-        char[] allChars = allInputAsString.toCharArray();
         StringBuilder sb = new StringBuilder("Kody ASCII: ");
 
+/*        String allInputAsString = String.join(" ", actionInput); // z powrotem calosc w Stringu
+        char[] allChars = allInputAsString.toCharArray();
         for (char znak : allChars) {
             sb.append((int) znak).append("d ");
         }
-
-        final String revertedAsciiValuesOfStrings = Stream.of("aaaa", "bddd", "c")
-                .flatMapToInt(String::chars)
-           //     .map(String::valueOf)
-                .mapToObj(StringBuilder::new)
-                .reduce(StringBuilder::append)
-                .map(StringBuilder::reverse)
-                .map(StringBuilder::toString)
-                .orElse("");
-        System.out.println(revertedAsciiValuesOfStrings);
-
 //        sb.deleteCharAt(sb.length() - 1); //skasowanie spacji na koncu
 //        sb.delete(sb.length() - 1, sb.length()); //skasowanie spacji na koncu
-        return sb.toString().trim(); // skasowanie spacji na koncu
+        return sb.toString().trim(); // skasowanie spacji na koncu*/
+
+        final String revertedAsciiValuesOfStrings = actionInput.stream()
+                .flatMapToInt(String::chars)
+                .mapToObj(String::valueOf)
+                .map(StringBuilder::new)
+                .map(x -> x.append("d "))
+                .reduce(StringBuilder::append)
+                .map(StringBuilder::toString)
+                .orElse("(nic nie podano)");
+        return sb.append(revertedAsciiValuesOfStrings).toString().trim();
     }
 }
--- src/main/java/pl/sda/git/impl/Cut3ActionAS.java
+++ src/main/java/pl/sda/git/impl/Cut3ActionAS.java
@@ -8,13 +8,14 @@ import java.util.stream.Collectors;
 public class Cut3ActionAS implements Action {
     public String doIt(List<String> actionInput) {
         actionInput = actionInput.stream()
-                .map(x -> {
+/*                .map(x -> {
                     try {
                         return x.substring(0, 3);
                     } catch (IndexOutOfBoundsException e) {
                         return x;
                     }
-                })
+                })*/
+                .map(x -> x.length() > 3 ? x.substring(0, 3) : x)
                 .collect(Collectors.toList());
         return String.join(" ", actionInput);
     }
