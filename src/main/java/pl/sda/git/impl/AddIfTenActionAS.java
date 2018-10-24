f76dca6ceab161d5dc32320c741e0559d360697b
arqarq <43341071+arqarq@users.noreply.github.com>
2018-10-24 23:25:08 +0200

    - poprawka w AddIfTenActionAS.java

--- src/main/java/pl/sda/git/impl/AddIfTenActionAS.java
+++ src/main/java/pl/sda/git/impl/AddIfTenActionAS.java
@@ -3,18 +3,19 @@ package pl.sda.git.impl;
 import pl.sda.git.Action;
 
 import java.util.List;
-import java.util.stream.LongStream;
+import java.util.stream.DoubleStream;
 
 public class AddIfTenActionAS implements Action {
     public String doIt(List<String> actionInput) {
-        LongStream longStream = actionInput.stream()
-                .filter(x -> x.matches("-?\\d+"))
-                .mapToLong(Integer::valueOf);
+        DoubleStream longStream = actionInput.stream()
+//                .filter(x -> x.matches("-?\\d+"))
+                .filter(x -> x.matches("^[+-]?\\d+(\\.\\d+)?$"))
+                .mapToDouble(Double::valueOf);
         if (actionInput.size() > 10) {
-            long result = longStream.sum();
+            double result = longStream.sum();
             return String.valueOf(result);
         }
-        long result = longStream.reduce(1, (x, y) -> x * y);
+        double result = longStream.reduce(1, (x, y) -> x * y);
         return String.valueOf(result);
     }
 }
